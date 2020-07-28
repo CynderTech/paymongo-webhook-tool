c1f9e823649e35313de409182d71773a40909118
Mico <mico@cynder.io>
2020-07-28 16:11:30 +0800

    feat: added input event handlers to generate form

--- components/Generate.form.js
+++ components/Generate.form.js
@@ -1,15 +1,34 @@
-export default () => (
-    <form>
-        <div className="flex flex-col pt-2">
-            <label className="text-base font-semibold" htmlFor="webhook_url">Webhook URL</label>
-            <input className="text-base border rounded h-8 p-2 mt-1" type="text" name="webhook_url" />
-        </div>
-        <div className="flex flex-col pt-4">
-            <label className="text-base font-semibold" htmlFor="secret_key">Secret Key</label>
-            <input className="text-base border rounded h-8 p-2 mt-1" type="text" name="secret_key" />
-        </div>
-        <button className="mt-4 p-2 w-full rounded bg-orange-600 text-white font-semibold" type="button">
-            Generate
-        </button>
-    </form>
-)
+import { useState } from 'react';
+
+export default () => {
+    const [webhookUrl, setWebhookUrl] = useState('');
+    const [secretKey, setSecretKey] = useState('');
+
+    return (
+        <form>
+            <div className="flex flex-col pt-2">
+                <label className="text-base font-semibold" htmlFor="webhook_url">Webhook URL</label>
+                <input
+                    className="text-base border rounded h-8 p-2 mt-1"
+                    type="text"
+                    name="webhook_url"
+                    value={webhookUrl}
+                    onChange={e => setWebhookUrl(e.target.value)}
+                />
+            </div>
+            <div className="flex flex-col pt-4">
+                <label className="text-base font-semibold" htmlFor="secret_key">Secret Key</label>
+                <input
+                    className="text-base border rounded h-8 p-2 mt-1"
+                    type="text"
+                    name="secret_key"
+                    value={secretKey}
+                    onChange={e => setSecretKey(e.target.value)}
+                />
+            </div>
+            <button className="mt-4 p-2 w-full rounded bg-orange-600 text-white font-semibold" type="button">
+                Generate
+            </button>
+        </form>
+    )
+}
