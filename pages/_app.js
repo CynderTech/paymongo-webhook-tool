33d24aa0e015213357ae4721480e7f4d997e2f28
Mico <mico@cynder.io>
2020-07-28 15:29:52 +0800

    feat: added result textarea for responses

--- pages/_app.js
+++ pages/_app.js
@@ -10,15 +10,21 @@ export default () => {
 
     return (
         <div className="flex h-screen justify-center items-center">
-            <div className="w-1/4 p-4 rounded border border-orange-600 shadow-md">
+            <div className="w-1/2 p-4 rounded border border-orange-600 shadow-md">
                 <h1 className="text-2xl text-center">PayMongo Webhook Tool</h1>
-                <ul className="flex border-b pt-4">
-                    <Tab name="generate" active={selectedTab} label="Generate" onSelect={setSelectedTab} />
-                    <Tab name="retrieve" active={selectedTab} label="Retrieve" onSelect={setSelectedTab} />
-                </ul>
-                <div className="w-full pt-4" style={{ minHeight: '216px' }}>
-                    {selectedTab === 'generate' && <GenerateForm />}
-                    {selectedTab === 'retrieve' && <RetrieveForm />}
+                <div className="flex justify-between w-full pt-4" style={{ minHeight: '273px' }}>
+                    <div className="w-full pr-2">
+                        <ul className="flex border-b pt-4">
+                            <Tab name="generate" active={selectedTab} label="Generate" onSelect={setSelectedTab} />
+                            <Tab name="retrieve" active={selectedTab} label="Retrieve" onSelect={setSelectedTab} />
+                        </ul>
+                        {selectedTab === 'generate' && <GenerateForm />}
+                        {selectedTab === 'retrieve' && <RetrieveForm />}
+                    </div>
+                    <div className="w-full pl-2">
+                        <h2 className="text-base font-semibold">Results</h2>
+                        <textarea className="w-full h-56 border" style={{ resize: 'none' }} readOnly />
+                    </div>
                 </div>
             </div>
         </div>
