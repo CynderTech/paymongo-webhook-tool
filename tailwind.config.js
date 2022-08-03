a161199fa16ff4b70f2e2a7ca805b6ac6493599c
Micah Andrew Bule <micah.bule@gmail.com>
2022-08-03 20:35:13 +0800

    chore: update tailwindcss to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 }
